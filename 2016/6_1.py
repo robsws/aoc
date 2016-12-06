92bdef2a9872e6ef0e31c594a92e3f2659ad5c4f
Robert Streeting <rstreeting@fxdeva11.factset.com>
2016-12-06 05:02:47 -0500

    Adding day 6 solutions

---
+++ 2016/6_1.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python
+file = open("6_input.txt",'r')
+import time
+
+positions = []
+for line in file:
+  line = line.rstrip()
+  if not positions:
+    for i in range(len(line)):
+      positions.append([])
+  for i,char in enumerate(line):
+    positions[i].append(char)
+
+def mode(char_list):
+  char_count = {}
+  for char in char_list:
+    if char in char_count:
+      char_count[char] += 1
+    else:
+      char_count[char] = 1
+  return reduce(lambda x,y: x if char_count[x] > char_count[y] else y, char_list)
+
+message = ''
+for position in positions:
+  message += mode(position)
+print(message)
