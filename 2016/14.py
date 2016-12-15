6007952664fa9a4986bee5a3a03c7db32990277b
Rob Streeting <robsws@gmail.com>
2016-12-15 08:53:22 +0000

    Completed part 2

--- 2016/14.py
+++ 2016/14.py
@@ -7,7 +7,8 @@ import time
 repeat3_regex = re.compile(r'000|111|222|333|444|555|666|777|888|999|aaa|bbb|ccc|ddd|eee|fff')
 repeat5_regex = re.compile(r'00000|11111|22222|33333|44444|55555|66666|77777|88888|99999|aaaaa|bbbbb|ccccc|ddddd|eeeee|fffff')
 
-salt = sys.argv[1]
+part = sys.argv[1]
+salt = sys.argv[2]
 
 keys = []
 pending_keys = []
@@ -16,7 +17,10 @@ index = 0
 while len(keys) < 100:
 
   plaintext = salt+str(index)
-  ciphertext = hashlib.md5(plaintext.encode('utf-8')).hexdigest()
+  hashtext = plaintext
+  number_of_hashes = 2017 if part == '2' else 1
+  for i in range(number_of_hashes):
+    hashtext = hashlib.md5(hashtext.encode('utf-8')).hexdigest()
 
   # Update counters
   keys_to_remove = []
@@ -28,7 +32,7 @@ while len(keys) < 100:
     pending_keys.remove(key)
 
   # Find strings of 5 same characters
-  matches = re.findall(repeat5_regex, ciphertext)
+  matches = re.findall(repeat5_regex, hashtext)
   if len(matches) > 0:
     for match in matches:
       chain = match
@@ -41,7 +45,7 @@ while len(keys) < 100:
         pending_keys.remove(key)
 
   # Find strings of 3 same characters
-  match = re.search(repeat3_regex, ciphertext)
+  match = re.search(repeat3_regex, hashtext)
   if match:
     chain = match.group(0)
     key = {
