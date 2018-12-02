2ef6fa841a91699ae4e5921b7ef3fd792d6fd5b3
Rob Streeting <robsws@gmail.com>
2018-12-02 21:16:56 +0000

    Adding day 2 solutions

--- 2018/solutions.py
+++ 2018/solutions.py
@@ -1,4 +1,5 @@
 import utils
+from collections import defaultdict
 
 # Day One Solutions
 def sum_frequencies(inputs):
@@ -21,8 +22,34 @@ def first_frequency_reached_twice(inputs):
                 frequency_history.add(current_frequency)
     return frequency_reached_twice
 
+# Day Two Solutions
+def box_checksum(inputs):
+    twos = 0
+    threes = 0
+    for box_id in inputs:
+        chars = defaultdict(int)
+        for char in box_id:
+            chars[char] += 1
+        if 2 in chars.values():
+            twos += 1
+        if 3 in chars.values():
+            threes += 1
+    return twos * threes
+
+def compare_box_ids(box1, box2):
+    diffs = [x for x in zip(box1, box2) if x[0] != x[1]]
+    return len(diffs) == 1
+
+def find_correct_boxes(inputs):
+    for box1 in inputs:
+        for box2 in inputs:
+            if box1 != box2 and compare_box_ids(box1, box2):
+                return ''.join([x for i,x in enumerate(box1) if x == box2[i]])
+    return "No correct boxes found."
+
 solution_list = [
-    [sum_frequencies, first_frequency_reached_twice]
+    [sum_frequencies, first_frequency_reached_twice],
+    [box_checksum, find_correct_boxes]
 ]
 
 def get_solver(day, part):
